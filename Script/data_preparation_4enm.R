343560a43fd3e11f74d09824e539970db022339b
marlonecobos <manubio13@gmailcom>
2026-03-04 20:22:25 -0500

    to do and data preparation for enm updated

--- Script/data_preparation_4enm.R
+++ Script/data_preparation_4enm.R
@@ -2,11 +2,17 @@ library(geodata)
 library(kuenm2)
 library(ENMeval)
 
+# set working directory
+#setwd("YOUR/DIRECTORY")
+
 # data
+## occurrence records from virtual niche
 sp_data <- read.csv("Results/data_virtual_species.csv")
 
+## area for model calibration
 marea <- vect("Data/vector/accessible_area.gpkg")
 
+## bioclimatic variables
 variables <- worldclim_global(var = "bio", res = 5, path = "Data")
 
 ## variables in M area
@@ -15,120 +21,88 @@ mvars <- crop(variables, marea, mask = TRUE)
 names(mvars) <- gsub("wc2.1_5m_", "", names(mvars))
 names(mvars) 
 
+
 # preparing data for models using kuenm2
 d <- prepare_data(algorithm = "maxnet", occ = sp_data[, c("x", "y")], x = "x",
                   y = "y", raster_variables = mvars[[c(1, 12)]], 
-                  n_background = 5000, features = c("l", "lq", "lqp"), 
-                  partition_method = "kfolds", 
+                  n_background = 5000,  # make sure 5000 does not exceed the number of pixels in the calibration area
+                  features = c("l", "lq", "lqp"),  
+                  partition_method = "kfolds", n_partitions = 4,
                   r_multiplier = c(0.1, 0.25, 0.5, 0.75, 1:5))
 
 dk <- d  # k will represent kflods in our next objects
 
-d
+dk
 
-d$formula_grid
 
-# blocks ENMeval
-## calibration data from the prepared_data object and separate presence and background records
-calib_occ <- d$data_xy[d$calibration_data$pr_bg == 1,] #Presences
-calib_bg <- d$data_xy[d$calibration_data$pr_bg == 0,] #Background
+# ENMeval blocks
+## Separate presence and background records
+calib_occ <- d$data_xy[d$calibration_data$pr_bg == 1, ]  # Presences
+calib_bg <- d$data_xy[d$calibration_data$pr_bg == 0, ]  # Background
 
-## apply spatial block partitioning using ENMeval
+## Apply spatial block partitioning using ENMeval
 enmeval_block <- get.block(occs = calib_occ, bg = calib_bg)
 
-
-# Identify unique spatial blocks
+## Identify unique spatial blocks
 id_blocks <- sort(unique(unlist(enmeval_block)))
 
-# Create a list of test indices for each spatial block
+## Create a list of test indices for each spatial block
 new_part_data <- lapply(id_blocks, function(i) {
-  # Indices of occurrence records in group i
+  ### Indices of occurrence records in group i
   rep_i_presence <- which(enmeval_block$occs.grp == i)
   
-  # Indices of background records in group i
+  ### Indices of background records in group i
   rep_i_bg <- which(enmeval_block$bg.grp == i)
-  # To get the right indices, we need to sum the number of records
+  
+  ### To get the right indices for background, 
+  ### we need to sum the total number of records to background indices
   rep_i_bg <- rep_i_bg + nrow(sp_data)
   
-  # Combine presence and background indices for the test set
+  ### Combine presence and background indices for the test set
   c(rep_i_presence, rep_i_bg)
 })
 
-# Assign names to each partition
+## Assign names to each partition
 names(new_part_data) <- paste0("Partition_", id_blocks)
 
-# Inspect the structure of the new partitioned data
-str(new_part_data)
+## Replace the original partition data with the new spatial blocks
+d_block <- d
+d_block$part_data <- new_part_data
+
+## Update the partitioning method to reflect the new approach
+d_block$partition_method <- "Blocks (ENMeval)"  # You can use any descriptive name
 
 
-# put it back to prepared_data object
-d$part_data <- new_part_data
+# ENMeval checkerboard
+## Apply checkerboard partitioning using ENMeval
+enmeval_check <- get.checkerboard(occs = calib_occ, envs = mvars[[c(1, 12)]], 
+                                  bg = calib_bg, aggregation.factor = c(4, 8))
 
-# Update the partitioning method to reflect the new approach
-d$partition_method <- "Spatial block (ENMeval)"  # You can use any descriptive name
+## Identify unique spatial blocks
+id_checks <- sort(unique(unlist(enmeval_check)))
 
-# Print the updated prepared_data object
-print(d)
+## Create a list of test indices for each spatial block
+new_part_check <- lapply(id_checks, function(i) {
+  ### Indices of occurrence records in group i
+  rep_i_presence <- which(enmeval_check$occs.grp == i)
+  
+  ### Indices of background records in group i
+  rep_i_bg <- which(enmeval_check$bg.grp == i)
+  
+  ### To get the right indices for background, 
+  ### we need to sum the total number of records to background indices
+  rep_i_bg <- rep_i_bg + nrow(sp_data)
+  
+  ### Combine presence and background indices for the test set
+  c(rep_i_presence, rep_i_bg)
+})
 
-# Exploring partitions in geography
-d_exp <- explore_partition_geo(data = d, raster_variables = mvars[[c(1, 12)]], 
-                               show_partitions = T)
+## Assign names to each partition
+names(new_part_check) <- paste0("Partition_", id_blocks)
 
-x11()
-plot(d_exp)
-plot(d_exp$Background)
-plot(d_exp$Presence)
+## Replace the original partition data with the new spatial blocks
+d_check <- d
+d_check$part_data <- new_part_check
 
-
-dk_exp <- explore_partition_geo(data = dk, raster_variables = mvars[[c(1, 12)]], 
-                                show_partitions = T)
-
-x11()
-plot(dk_exp)
-plot(dk_exp$Background)
-plot(dk_exp$Presence)
-
-
-# model calibration
-cal <- calibration(data = d, error_considered = 5, remove_concave = FALSE,
-                   proc_for_all = TRUE)
-
-calk <- calibration(data = dk, error_considered = 5, remove_concave = FALSE,
-                    proc_for_all = TRUE)
-
-View(cal$selected_models)
-
-# response curves for partitions
-partition_response_curves(calibration_results = cal, modelID = 19)
-
-partition_response_curves(calibration_results = calk, modelID = 19)
-
-# saving results
-saveRDS(d, "Results/prepared_data.RDS")
-saveRDS(cal, "Results/calibration_results.RDS")
-saveRDS(dk, "Results/prepared_datak.RDS")
-saveRDS(calk, "Results/calibration_resultsk.RDS")
-writeRaster(d_exp, "Results/prep_data_exploration.tiff")
-writeRaster(dk_exp, "Results/prep_data_explorationk.tiff")
-
-# read the ovjects
-cal <- readRDS("Results/calibration_results.RDS")
-calk <- readRDS("Results/calibration_resultsk.RDS")
-
-# selected models
-cal$selected_models
-calk$selected_models
-
-# fit selected models
-fm <- fit_selected(calibration_results = cal, n_replicates = 1)
-fmk <- fit_selected(calibration_results = calk, n_replicates = 1)
-
-# response curves per model but for all variables
-all_response_curves(models = fm, modelID = "Model_19")
-all_response_curves(models = fmk, modelID = "Model_13")
-
-# bivariate responses
-fm$thresholds$Model_19
-
-bivariate_response(models = fmk, variable1 = "bio_1", variable2 = "bio_12", 
-                   modelID = "Model_19")
+## Update the partitioning method to reflect the new approach
+d_check$partition_method <- "Checkerboard (ENMeval)"  # You can use any descriptive name
---
+++ TO_DO.md
@@ -0,0 +1,37 @@
+## To do
+
+### Session 2026-03-05:
+
+1. Define calibration areas for models by checking virtual niche suitable areas.
+    - Predict suitable areas in geography (North America).
+    - Overlap areas of interest (Great Plains and Sierra Madre).
+    - Define area for model calibration based on areas of interest and a limit that makes sense geographically.
+    - Mask predictions with accessible areas.
+
+2. Prepare data for ENM 
+    - Generate occurrence points from masked prediction.
+    - Mask raster variables with calibration area.
+    - Prepare data for ENM using kuenm2 prepare_data(), see [vignette](https://marlonecobos.github.io/kuenm2/articles/prepare_data.html)
+        - Use 4 kfolds for data partitioning.
+        - Use other parameter settings included in the script.
+    - Prepare additional sets of data with other partitioning methods.
+        - Prepare block partitioning.
+        - Add prepared blocks into prepared data partitions.
+        - Prepare checkerboard partitioning?
+        - Add checkerboard into prepared data partitions?
+
+3. Explore prepared data as in the kuenm2 [vignette](https://marlonecobos.github.io/kuenm2/articles/prepare_data.html)
+4. Run model calibration for all prepared data sets see [calibration vignette](https://marlonecobos.github.io/kuenm2/articles/model_calibration.html).
+    - Make sure the calibration process runs with the argument `remove_concave = FALSE`.
+
+5. Explore training partition effects on response curves as indicated in the previous vignette.
+6. Fit and explore selected models ([fit models vignette](https://marlonecobos.github.io/kuenm2/articles/model_exploration.html)) 
+    - Fit models selected.
+    - Explore response curves.
+        - Check single response curves.
+        - Explore bivariate responses.
+        - Change the range for new data in response curves, so that it includes environments in North America.
+    - Evaluate and show variable contribution to models.
+    
+7. Project fitted models to North America see [vignette](https://marlonecobos.github.io/kuenm2/articles/model_predictions.html).
+
